618f10c8ec6cc25a1a9e0637782b957ff0b0eb7d
Kamalshakoor <kamalshakoor29022000@gmail.com>
2024-10-18 01:42:13 +0500

    added toastify for notifications

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Signup from './components/Auth/Signup';
 import Login from './components/Auth/Login';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';    
+
 function App() {
  
   return <>
@@ -13,6 +16,7 @@ function App() {
       <Route path="/signup" element={<Signup />} />
     </Routes>
   </Router>
+  <ToastContainer />
   </>;
 }
 
